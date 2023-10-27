e326f79fa9b0e5f83adb4e34cc83d6d2d57424c9
varsha <varshaagrawal045@gmail.com>
2023-10-27 15:58:06 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555; 
+
+export const mongoDBURL = "mongodb+srv://varshaagrawal045:Varsha@123@books-store-mern.ty9kujo.mongodb.net/books-Collection?retryWrites=true&w=majority";
