0bafdb31e6877673db2cbbe86de309f76b6d9b9c
varsha <varshaagrawal045@gmail.com>
2023-12-25 12:53:55 +0530

    file modified

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -30,7 +30,7 @@ const ShowBook = () => {
          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
             <div className="my-4">
                 <span className='text-xl mr-4 text-gray-500'>Id</span>
-                {/* <span>{book._id}</span> */}
+                <span>{book._id}</span>
             </div>
             <div className="my-4">
                 <span className='text-xl mr-4 text-gray-500'>Title</span>
